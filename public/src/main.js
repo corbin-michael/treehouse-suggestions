718bceda8e4682c28d98beedfb110c79600353cf
Corbin Jensen <cjensen@carbonhouse.com>
2017-03-01 16:17:51 -0500

    fixes

--- public/src/main.js
+++ public/src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
+//import Vuex from 'vuex'
 import VueFire from 'vuefire'
 import VueRouter from 'vue-router'
 import App from './App'
@@ -9,7 +9,7 @@ import Login from './components/Login'
 import CreateAccount from './components/CreateAccount'
 import Profile from './components/Profile'
 
-Vue.use(Vuex)
+//Vue.use(Vuex)
 Vue.use(VueFire)
 Vue.use(VueRouter)
 
